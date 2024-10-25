06870e49274e3e4a7ec410adba6b363aba5a6443
Sebastian <fsinsfran@gmail.com>
2024-10-25 14:02:16 -0300

    Cambio de funciones a metodos usando una clase Topic

--- src/models/temaModel.js
+++ src/models/temaModel.js
@@ -1,31 +1,24 @@
-//Utilizamos Pool para realizar la configuracion de la base de datos
-import { Pool } from "pg";
-
-const pool = new Pool({
-    user : 'sebastian',
-    host : 'localhost',
-    database : 'crud_voting',
-    password : '7501095',
-    port : '5432'
-});
-
+import pool from "../postgresClient.js";
 //Hacemos el modelo para interactuar con la base de datos 
 //Funciones para el crud
-module.exports = {
-    obtenerTemas : async () => {
+class Topic {
+    static async getTopics() {
         const res = await pool.query('SELECT * FROM temas ORDER BY votos DESC');
         return res.rows;
-    },
+    }
 
-    crearTema : async (titulo) => {
-        await pool.query('INSERT INTO temas (titulo) values ($1)', [titulo]);
-    },
+    
+    static async createTopic(titulo) {
+        await pool.query('INSERT INTO temas (titulo) VALUES ($1)', [titulo]);
+    }
 
-    votarTema : async (id) => {
+    static async votarTema(id) {
         await pool.query('UPDATE temas SET votos = votos + 1 WHERE id = $1', [id]);
-    },
+    }
 
-    eliminarTema : async (id) => {
+    static async eliminarTema(id) {
         await pool.query('DELETE FROM temas WHERE id = $1', [id]);
     }
-};
+}
+
+export default Topic;
