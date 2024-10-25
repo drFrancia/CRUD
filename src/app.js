6dddb51ce6e64337e59e369dfcfdee24bc7c60cb
Sebastian <fsinsfran@gmail.com>
2024-10-25 13:47:39 -0300

    Config del entrono de vistas en app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
 /*Importamos express*/
+import path from 'path'
 import express from 'express';
 /* importamos bodyparser para manejar los cuerpos de las solicitudes */
 import bodyParser from 'body-parser';
 /* Importamos methodoverride para el uso de put y delete en formularios que no permiten estos metodos por defecto*/
 import methodOverride from 'method-override';
+import { getAllTopics } from './controllers/controladorTemas.js';
+import { fileURLToPath } from 'url';
+
 /* Instanciamos express */
 const app = express();
 
 /*Configuramos la aplicacion*/
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(methodOverride('method'));
-app.set('view engine', ejs);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+//Topic controller
+app.get('/', getAllTopics);
+
+
 
 /* Definimos en que puerto va a correr nuestra app */
 const port = process.env.PORT || 3000;
@@ -19,4 +39,4 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 /* Mostramos mensaje de servidor escuchando */
-app.listen(port, () => {console.log(`Servidor corriendo en el puerto ${port}`);});
+app.listen(port, () => {console.log(`Servidor corriendo en el puerto http://localhost:${port}`);});
