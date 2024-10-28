15319e8d440d4fb7a24f3123105b708ab6fd9aa5
Sebastian <fsinsfran@gmail.com>
2024-10-28 17:44:00 -0300

    Add 'postTopic' controller

--- src/controllers/controladorTemas.js
+++ src/controllers/controladorTemas.js
@@ -1,13 +1,24 @@
 //importamos models quien es quien contiene las funciones para interactuar con la db
+import { render } from 'ejs';
 import Topic from '../models/temaModel.js';
 
 
 export const getAllTopics = async(req, res) => {
     try {
         const topics = await Topic.getTopics();
-        console.log(topics)
+        console.log("Se agrego un nuevo tema!.")
         res.render('index',{topics});
     } catch (error) {
-        console.log("Error al obtener todos los temas." + error);
+        console.error("Error al obtener todos los temas." + error);
+    }
+};
+//
+export const postTopic = async (req, res) => {
+    try {
+        const { titulo } = req.body;
+        await Topic.createTopic(titulo);
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error al crear un nuevo tema: " + error);
     }
 };
