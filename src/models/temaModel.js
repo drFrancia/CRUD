0b8f164e0ee1f04b996c2aa48da1a7c9e2d5f60c
Sebastian <fsinsfran@gmail.com>
2024-10-28 18:59:09 -0300

    Renaming attributes in queries

--- src/models/temaModel.js
+++ src/models/temaModel.js
@@ -7,15 +7,15 @@ class Topic {
         return res.rows;
     }
 
-    static async createTopic(tittle) {
-        await pool.query('INSERT INTO topics (tittle) VALUES ($1)', [tittle]);
+    static async createTopic(title) {
+        await pool.query('INSERT INTO topics (title) VALUES ($1)', [title]);
     }
 
     static async voteTopic(id) {
         await pool.query('UPDATE topics SET votes = votes + 1 WHERE id = $1', [id]);
     }
 
-    static async eliminarTema(id) {
+    static async deleteTopic(id) {
         await pool.query('DELETE FROM topics WHERE id = $1', [id]);
     }
 }
