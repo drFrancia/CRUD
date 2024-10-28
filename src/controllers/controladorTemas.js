6b8068d6d7281b0e3ac73328556692d602f97307
Sebastian <fsinsfran@gmail.com>
2024-10-28 18:13:00 -0300

    Add 'voteATopic' controller

--- src/controllers/controladorTemas.js
+++ src/controllers/controladorTemas.js
@@ -6,7 +6,6 @@ import Topic from '../models/temaModel.js';
 export const getAllTopics = async(req, res) => {
     try {
         const topics = await Topic.getTopics();
-        console.log("Se agrego un nuevo tema!.")
         res.render('index',{topics});
     } catch (error) {
         console.error("Error al obtener todos los temas." + error);
@@ -21,4 +20,14 @@ export const postTopic = async (req, res) => {
     } catch (error) {
         console.error("Error al crear un nuevo tema: " + error);
     }
+};
+
+export const voteATopic = async (req, res) => {
+    try{
+        const { id } = req.params;
+        await Topic.voteTopic(id);
+        res.redirect("/");
+    }  catch(error) {
+        console.log("Error al votar por el tema.");
+    };
 };
