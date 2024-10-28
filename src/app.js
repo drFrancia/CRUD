4b310654d74ec5afb8a544bea005746bd0674e82
Sebastian <fsinsfran@gmail.com>
2024-10-28 19:00:26 -0300

    Add a new controler '/topics/:id'

--- src/app.js
+++ src/app.js
@@ -5,15 +5,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 /* Importamos methodoverride para el uso de put y delete en formularios que no permiten estos metodos por defecto*/
 import methodOverride from 'method-override';
-import { getAllTopics, postTopic, voteATopic } from './controllers/controladorTemas.js';
+import { deleteATopic, getAllTopics, postTopic, voteATopic } from './controllers/controladorTemas.js';
 import { fileURLToPath } from 'url';
 
 /* Instanciamos express */
 const app = express();
 
+/* Configuramos el middleware para que maneje los datos json */
+app.use(express.json());
+
 /*Configuramos la aplicacion*/
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(methodOverride('method'));
+app.use(methodOverride('_method'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -31,14 +34,13 @@ app.set('view engine', 'ejs');
 app.get('/', getAllTopics);
 app.post('/topics', postTopic);
 app.post('/topics/:id/votes', voteATopic);
+app.delete('/topics/:id', deleteATopic);
 
 
 
 /* Definimos en que puerto va a correr nuestra app */
 const port = process.env.PORT || 3000;
 
-/* Configuramos el middleware para que maneje los datos json */
-app.use(express.json());
 
 /* Mostramos mensaje de servidor escuchando */
 app.listen(port, () => {console.log(`Servidor corriendo en el puerto http://localhost:${port}`);});
